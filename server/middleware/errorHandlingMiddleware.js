b7818ab7a58afa3b7754f7b5cc082afa245e5904
Mohd88umar <214827861+mohdumar-mern-dev@users.noreply.github.com>
2025-10-18 11:52:55 +0530

    refactor: improved error handling with validation, cast, and operational error support

--- server/middleware/errorHandlingMiddleware.js
+++ server/middleware/errorHandlingMiddleware.js
@@ -1,5 +1,26 @@
 
 export const errorHandling = (err, req, res, next) => {
     console.error(err.stack);
-    res.status(err.statusCode || 500).json({ message: err.message || 'Internal Server Error' });
-  }
+
+    if (err.name === 'ValidationError') {
+      const errors = Object.values(err.errors).map((error) => error.message);
+      res.status(400).json({ message: errors });
+    } else if (err.name === 'CastError') {
+      res.status(400).json({ message: 'Invalid ID format' });
+    } else {
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  };
+  
+  export const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({ message: err.message });
+    } 
+    
+    console.error(err);
+
+    return res.status(500).json({ message: 'Internal Server Error' });
+  };
+  
