f9813c7a99c320b371b1cf804c72ac81e2138baa
Mohd88umar <214827861+mohdumar-mern-dev@users.noreply.github.com>
2025-10-28 12:36:38 +0530

    Added to metadeta for dynamic title

---
+++ client/app/products/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const metadata = {
+  title: "Products | Eco Bazaar",
+  description: "Browse the latest trending eco-friendly products.",
+};
+
+const Products = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
